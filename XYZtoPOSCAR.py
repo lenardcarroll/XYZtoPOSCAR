d467fdf4de2f97ab96d18d15e7d101ce63da0d4a
Lenard Carroll <86952266+lenardcarroll@users.noreply.github.com>
2022-02-20 20:39:23 +0200

    Update XYZtoPOSCAR.py

--- XYZtoPOSCAR.py
+++ XYZtoPOSCAR.py
@@ -17,6 +17,54 @@ for i in range(1,4):
 VectorsAdj = []
 for i in Vectors:
     VectorsAdj.append([float(i[0])*ScaleFactor,float(i[1])*ScaleFactor,float(i[2])*ScaleFactor])
+FixedVals = content[4]
+commapos = []
+for i in range(len(FixedVals)):
+    if FixedVals[i]==',':
+        commapos.append(i)
+
+FRange = []
+
+for i in range(len(commapos)):
+    if i==0:
+        x = FixedVals[:commapos[i]]
+        if '-' in x:
+            xindex = x.index('-')
+            firstval = int(x[:xindex])
+            secondval = int(x[xindex+1:])
+            for i in range(firstval-1,secondval):
+                FRange.append(i)
+        else:
+            FRange.append(int(x)-1)
+    else:
+        x = FixedVals[commapos[i-1]+1:commapos[i]]
+        if '-' in x:
+            xindex = x.index('-')
+            firstval = int(x[:xindex])
+            secondval = int(x[xindex+1:])
+            for i in range(firstval-1,secondval):
+                FRange.append(i)
+        else:
+            FRange.append(int(x)-1)
+x = FixedVals[commapos[len(commapos)-1]+1:]
+if '-' in x:
+    xindex = x.index('-')
+    firstval = int(x[:xindex])
+    secondval = int(x[xindex+1:])
+    for i in range(firstval-1,secondval):
+        FRange.append(i)
+else:
+    FRange.append(int(x)-1)
+if len(commapos) == 0:
+    if '-' in FixedVals:
+        xindex = FixedVals.index('-')
+        firstval = int(FixedVals[:xindex])
+        secondval = int(FixedVals[xindex+1:])
+        for i in range(firstval-1,secondval):
+            FRange.append(i)
+    else:
+        FRange.append(int(FixedVals)-1)
+
 
 g = open(args.input2,"r")
 content = g.readlines()
@@ -28,9 +76,9 @@ for i in range(2,len(content)):
 
 h = open(args.output,"w")
 print("XYZ to POSCAR",file=h)
-print(ScaleFactor,file=h)
+print("%0.16f" % ScaleFactor,file=h)
 for i in Vectors:
-    print(i[0],i[1],i[2],file=h)
+    print("%0.16f" % float(i[0]),"%0.16f" % float(i[1]),"%0.16f" % float(i[2]),file=h)
 AtomList = []
 AtomNum = []
 for i in range(len(Atoms)):
@@ -58,5 +106,8 @@ for i in AtomNum:
 
 for i in range(AtomTot):
     MatrixMult = np.matmul(AtomCoords[i],np.linalg.inv(VectorsAdj))
-    print(MatrixMult[0], MatrixMult[1],MatrixMult[2],file=h)   
+    if i in FRange:
+        print('%0.16f' % MatrixMult[0], '%0.16f' % MatrixMult[1],'%0.16f' % MatrixMult[2],'F','F','F',file=h)  
+    else:
+        print('%0.16f' % MatrixMult[0], '%0.16f' % MatrixMult[1],'%0.16f' % MatrixMult[2],'T','T','T',file=h)  
 h.close()
